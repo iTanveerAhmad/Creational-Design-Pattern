1ab3bdef92a27f904dfe2472885d9c012ad8c778
ghairatCS22 <talktanveerkhan@outlook.com>
2022-03-21 12:33:51 +0100

    Initial Commit

---
+++ src/main/java/com/example/creationalpattern/CreationalpatternApplication.java
@@ -0,0 +1,13 @@
+package com.example.creationalpattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreationalpatternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreationalpatternApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/creationalpattern/CreationalpatternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.creationalpattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreationalpatternApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
